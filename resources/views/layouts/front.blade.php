e4f16ef1ef5a1f17cc569edbec846d2ea30177ec
4cece <cece4487@gmail.com>
2021-09-13 14:06:39 +0200

    navbar

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -4,12 +4,14 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="css/app.css">
   <title>Document</title>
 </head>
-<body>
+<body class="bg-bo">
 
   @include('includes.header')
   
   @yield('content')
+  <script src="https://kit.fontawesome.com/cd2afb3baf.js" crossorigin="anonymous"></script>
 </body>
 </html>
