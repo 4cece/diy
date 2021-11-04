d066eb958cb85d1872c2cb9533ce954867e50982
4cece <celine.montlouis-calixte@gretasudchampagne.com>
2021-11-04 17:11:57 +0100

    formulaire v1 fini

--- resources/views/pages/receipeForm.blade.php
+++ resources/views/pages/receipeForm.blade.php
@@ -26,7 +26,7 @@
 
     {{-- LES INGREDIENTS --}}
     <label for="ingredient">Ingrédient</label>
-    <select name="ingredient" id="ingredient">
+    <select name="ingredient" id="ingredient" >
         @foreach ($ingredients as $ingredient)
         <option value="{{$ingredient->id}}">{{$ingredient->name}}</option>
         @endforeach
@@ -34,12 +34,15 @@
 
     <div id="content"></div>
     
-    <button id="btnIngre">ajouter un ingrédient</button><br>
+    <button id="btnIngre" type="">ajouter un ingrédient</button><br>
 
     
     {{-- LES ETAPES DE PREPARATION --}}
     <label for="step">Préparation</label>
-    <input type="text" name="step"><br>
+    <input type="text" name="step" >
+    <div id="divstep"></div>
+
+    <button id="btnstep">Ajouter une étape</button><br>
 
     {{-- LES INFO COMPLEMENTAIRES --}}
 
@@ -57,40 +60,71 @@
     <button type="submit">Créer votre recette</button>
 
 </form>
+
+
 <script type="text/javascript">
     // Formulaire de création de recette
 // les variables
 
-    // Le bouton
+    // Le bouton pour ajouter un ingrédient
     let btn = document.querySelector('#btnIngre'); 
 
-    // la div
+    // Le bouton pour ajouter un étape de préparation
+    let btnstep = document.querySelector('#btnstep');
+
+    // la div des ingrédients
     let content = document.querySelector('#content');
 
-    let options = [
+    // la div des étapes de préparation
+    let divstep = document.querySelector('#divstep');
+
+
+   
+
+    btn.addEventListener("click", function(event){
+        // pour éviter d'envoyer le formulaire
+        event.preventDefault();
+
+        // Création des tableaux d'ingrédient
+        let options = [
         @foreach ($ingredients as $ingredient)
         ["{{$ingredient->id}}","{{$ingredient->name}}"],
         @endforeach
-    ]
+        ];
 
-    // console.log(options[0][0]);
-btn.addEventListener("click", function(){
+        //  création du Select
+        let select = document.createElement('select');
+        select.name= 'ingredient';
+        select.id= 'ingredient';
 
-    // alert('test');
-    //  création du Select
-    // let select = document.createElement('select');
-    // select.name= 'ingredient';
-    // select.id= 'ingredient';
+        content.appendChild(select);
 
-    // content.appendchil(select);
+        // creation des options
+        
+        for(var i= 0; i < options.length; i++){
+            let option = document.createElement('option');
+            option.value = options[i][0];
+            option.text= options[i][1];
 
-    // creation des options
-    // let option = document.createElement('option');
-    // option.value = "test";
-    // option.text = "test";
-
-    // select.appendchil(option);
-
+            select.appendChild(option); 
+        }
 })
+
+    btnstep.addEventListener('click', function(event){
+        // pour éviter d'envoyer le formulaire
+        event.preventDefault();
+
+        let input = document.createElement('input');
+        input.name = 'step';
+        input.type = 'text';
+        input.style = 'width:80%';
+        input.placeholder = 'étape de préparation'
+
+        divstep.appendChild(input);
+    })
+
+
+
+
 </script>
 @endsection
